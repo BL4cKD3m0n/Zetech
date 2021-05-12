20fad051a4059888094f45a80960428369f1456d
Yeral-cmd <hgva.1997@gmail.com>
2021-05-12 16:20:28 -0500

    controles
    prueba de controles

--- Proyecto Unity/DDV wis/Assets/PlayerControls.cs
+++ Proyecto Unity/DDV wis/Assets/PlayerControls.cs
@@ -5,11 +5,46 @@ using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
+    
     public float speed = 10f;
+    public float rotationSpeed = 15f;
+    Transform cameraObject;
+    Rigidbody playerRigidbody;
     private Vector2 movementInput;
+    private void Awake()
+    {
+        cameraObject = Camera.main.transform;
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
     private void Update()
     {
-        transform.Translate(new Vector3(movementInput.x, 0, movementInput.y) * speed * Time.deltaTime);
+        Vector3 moveDirection;//direccion
+        moveDirection = cameraObject.forward * movementInput.y;
+        moveDirection = moveDirection + cameraObject.right * movementInput.x;
+        moveDirection.Normalize();
+        moveDirection.y = 0;
+        moveDirection = moveDirection * speed;
+
+        Vector3 movementVelocity = moveDirection;
+        playerRigidbody.velocity = movementVelocity;
+
+       
+
+        Vector3 targetDirection = Vector3.zero;//rotacion
+
+        targetDirection = cameraObject.forward * movementInput.y;
+        targetDirection = targetDirection + cameraObject.right * movementInput.x;
+        targetDirection.Normalize();
+        targetDirection.y = 0;
+
+        if (targetDirection == Vector3.zero)
+        {
+            targetDirection = transform.forward;
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        transform.rotation = playerRotation;
     }
     public void OnMove(InputAction.CallbackContext ctx)=> movementInput = ctx.ReadValue<Vector2>();
 }
